9695bf4545d3e2b2eb0306f9f441f67a41297b0f
Richard Huang <hxr.richard@gmail.com>
2021-12-23 13:23:09 -0500

    solved 34 with binary search, 208. implemented trie with dictionaries (Hash) and begin 212 word search II

---
+++ 208.implement-trie-prefix-tree.py
@@ -0,0 +1,176 @@
+#
+# @lc app=leetcode id=208 lang=python3
+#
+# [208] Implement Trie (Prefix Tree)
+#
+# https://leetcode.com/problems/implement-trie-prefix-tree/description/
+#
+# algorithms
+# Medium (56.18%)
+# Likes:    6051
+# Dislikes: 83
+# Total Accepted:    511.2K
+# Total Submissions: 907.3K
+# Testcase Example:  '["Trie","insert","search","search","startsWith","insert","search"]\n' +
+# '[[],["apple"],["apple"],["app"],["app"],["app"],["app"]]'
+#
+# A trie (pronounced as "try") or prefix tree is a tree data structure used to
+# efficiently store and retrieve keys in a dataset of strings. There are
+# various applications of this data structure, such as autocomplete and
+# spellchecker.
+#
+# Implement the Trie class:
+#
+#
+# Trie() Initializes the trie object.
+# void insert(String word) Inserts the string word into the trie.
+# boolean search(String word) Returns true if the string word is in the trie
+# (i.e., was inserted before), and false otherwise.
+# boolean startsWith(String prefix) Returns true if there is a previously
+# inserted string word that has the prefix prefix, and false otherwise.
+#
+#
+#
+# Example 1:
+#
+#
+# Input
+# ["Trie", "insert", "search", "search", "startsWith", "insert", "search"]
+# [[], ["apple"], ["apple"], ["app"], ["app"], ["app"], ["app"]]
+# Output
+# [null, null, true, false, true, null, true]
+#
+# Explanation
+# Trie trie = new Trie();
+# trie.insert("apple");
+# trie.search("apple");   // return True
+# trie.search("app");     // return False
+# trie.startsWith("app"); // return True
+# trie.insert("app");
+# trie.search("app");     // return True
+#
+#
+#
+# Constraints:
+#
+#
+# 1 <= word.length, prefix.length <= 2000
+# word and prefix consist only of lowercase English letters.
+# At most 3 * 10^4 calls in total will be made to insert, search, and
+# startsWith.
+#
+#
+#
+
+# @lc code=start
+from typing import Dict, Tuple
+from enum import Enum
+
+approaches = Enum("approaches", "NAIVE FAST")
+APPROACH = approaches.FAST
+
+
+class Trie:
+    if APPROACH == approaches.FAST:
+        trie: Dict
+
+        def __init__(self):
+            self.trie: Dict = {}
+
+        def insert(self, word: str) -> None:
+            trie = self.trie
+            for c in word:
+                if c not in trie:
+                    trie[c] = {}
+                trie = trie[c]
+            trie["-"] = True
+
+
+        def search(self, word: str) -> bool:
+            trie = self.trie
+            for c in word:
+                if c not in trie:
+                    return False
+                trie = trie[c]
+            return "-" in trie
+
+        def startsWith(self, prefix: str) -> bool:
+            trie = self.trie
+            for c in prefix:
+                if c not in trie:
+                    return False
+                trie = trie[c]
+            return True
+
+    elif APPROACH == approaches.NAIVE:
+
+        def __init__(self):
+            self.tries: Dict[Tuple[Trie, bool]] = {}
+
+        def insert(self, word: str) -> None:
+            if not word:
+                return
+
+            c, rem = word[0], word[1:]
+            end = False
+            if c not in self.tries:
+                # create a new trie
+                trie = Trie()
+            else:
+                trie, end = self.tries[c]
+
+            if rem:
+                # make previous ending mark persistence with insert
+                self.tries[c] = (trie, False or end)
+            else:
+                # 1 indicating that this is a end of a word
+                self.tries[c] = (trie, True)
+            trie.insert(rem)
+
+        def search(self, word: str) -> bool:
+            if not word:
+                return True
+
+            c, rem = word[0], word[1:]
+            if c not in self.tries:
+                return False
+
+            trie, end = self.tries[c]
+            if rem:
+                return trie.search(rem)
+            else:
+                return end and trie.search(rem)
+
+        def startsWith(self, prefix: str) -> bool:
+            if not prefix:
+                return True
+
+            c, rem = prefix[0], prefix[1:]
+            if c not in self.tries:
+                return False
+
+            return self.tries[c][0].startsWith(rem)
+
+
+# Your Trie object will be instantiated and called as such:
+# obj = Trie()
+# obj.insert(word)
+# param_2 = obj.search(word)
+# param_3 = obj.startsWith(prefix)
+# @lc code=end
+
+
+def main():
+    trie = Trie()
+
+    trie.insert("app")
+    trie.insert("apple")
+
+    ans = trie.search("apps")  # return True
+    print(ans)
+    ans = trie.search("app")  # return False
+    print(ans)
+
+
+if __name__ == "__main__":
+    main()
---
+++ 34.find-first-and-last-position-of-element-in-sorted-array.py
@@ -0,0 +1,93 @@
+#
+# @lc app=leetcode id=34 lang=python3
+#
+# [34] Find First and Last Position of Element in Sorted Array
+#
+# https://leetcode.com/problems/find-first-and-last-position-of-element-in-sorted-array/description/
+#
+# algorithms
+# Medium (39.10%)
+# Likes:    8429
+# Dislikes: 259
+# Total Accepted:    919.7K
+# Total Submissions: 2.3M
+# Testcase Example:  '[5,7,7,8,8,10]\n8'
+#
+# Given an array of integers nums sorted in non-decreasing order, find the
+# starting and ending position of a given target value.
+#
+# If target is not found in the array, return [-1, -1].
+#
+# You must write an algorithm with O(log n) runtime complexity.
+#
+#
+# Example 1:
+# Input: nums = [5,7,7,8,8,10], target = 8
+# Output: [3,4]
+# Example 2:
+# Input: nums = [5,7,7,8,8,10], target = 6
+# Output: [-1,-1]
+# Example 3:
+# Input: nums = [], target = 0
+# Output: [-1,-1]
+#
+#
+# Constraints:
+#
+#
+# 0 <= nums.length <= 10^5
+# -10^9 <= nums[i] <= 10^9
+# nums is a non-decreasing array.
+# -10^9 <= target <= 10^9
+#
+#
+#
+
+from typing import List
+
+# @lc code=start
+class Solution:
+    def searchRange(self, nums: List[int], target: int) -> List[int]:
+        def bs_left(nums, target):
+            low, high = 0, len(nums) - 1
+            while low <= high:
+                mid = (low  + high) //2
+                # looking for highest number less than target
+                if nums[mid] < target:
+                    low = mid + 1
+                else:
+                    # even if they equal, high is set one below
+                    high = mid - 1
+            return low # the higher of the two
+
+        def bs_right(nums, target):
+            low, high = 0, len(nums) - 1
+            while low <= high:
+                mid = (low  + high) //2
+                # looking for the lowest number higher than target
+                if nums[mid] <= target:
+                    low = mid + 1
+                else:
+                    # even if they equal, high is set one below
+                    high = mid - 1
+            return high # the higher of the two
+
+        low, high = bs_left(nums, target), bs_right(nums, target)
+        return [low, high] if low <= high else [-1, -1]
+
+
+# @lc code=end
+
+
+def main():
+    sol = Solution()
+    ans = sol.searchRange(
+        # [5, 7, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 10], 8
+        # [1,3], 1
+        [1, 2, 3], 1,
+    )
+    print(ans)
+
+
+if __name__ == "__main__":
+    main()
