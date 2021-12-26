2d2347c650bd22aeebc7323d91f0e1c5cef6c1a6
Richard Huang <hxr.richard@gmail.com>
2021-12-26 13:10:56 -0500

    solved 844 backspace string compare with string build, need to review two pointer solution

---
+++ 844.backspace-string-compare.py
@@ -0,0 +1,141 @@
+#
+# @lc app=leetcode id=844 lang=python3
+#
+# [844] Backspace String Compare
+#
+# https://leetcode.com/problems/backspace-string-compare/description/
+#
+# algorithms
+# Easy (47.46%)
+# Likes:    3316
+# Dislikes: 153
+# Total Accepted:    359.6K
+# Total Submissions: 757.3K
+# Testcase Example:  '"ab#c"\n"ad#c"'
+#
+# Given two strings s and t, return true if they are equal when both are typed
+# into empty text editors. '#' means a backspace character.
+#
+# Note that after backspacing an empty text, the text will continue empty.
+#
+#
+# Example 1:
+#
+#
+# Input: s = "ab#c", t = "ad#c"
+# Output: true
+# Explanation: Both s and t become "ac".
+#
+#
+# Example 2:
+#
+#
+# Input: s = "ab##", t = "c#d#"
+# Output: true
+# Explanation: Both s and t become "".
+#
+#
+# Example 3:
+#
+#
+# Input: s = "a#c", t = "b"
+# Output: false
+# Explanation: s becomes "c" while t becomes "b".
+#
+#
+#
+# Constraints:
+#
+#
+# 1 <= s.length, t.length <= 200
+# s and t only contain lowercase letters and '#' characters.
+#
+#
+#
+# Follow up: Can you solve it in O(n) time and O(1) space?
+#
+#
+
+# @lc code=start
+from typing import List
+
+from enum import Enum
+
+approaches = Enum("approaches", "LAZY_LST POINTERS")
+APPROACH = approaches.POINTERS
+
+
+class Solution:
+    def backspaceCompare(self, s: str, t: str) -> bool:
+        if APPROACH == approaches.LAZY_LST:
+            return self.backspaceCompare_lazy_lst(s, t)
+        elif APPROACH == approaches.POINTERS:
+            return self.backspaceCompare_pointers(s, t)
+    def backspaceCompare_pointers(self, s: str, t: str) -> bool:
+        i = len(s) - 1			# Traverse from the end of the strings
+        j = len(t) - 1
+
+        skipS = 0              # The number of backspaces required till we arrive at a valid character
+        skipT = 0
+
+        while i >= 0 or j >= 0:
+            while i >= 0:					# Ensure that we are comparing a valid character in S
+                if s[i] == "#" :
+                    skipS += 1				# If not a valid character, keep times we must backspace.
+                    i = i - 1
+
+                elif skipS > 0:
+                    skipS -= 1				# Backspace the number of times calculated in the previous step
+                    i = i - 1
+
+                else:
+                    break
+
+            while j >= 0:					# Ensure that we are comparing a valid character in T
+                    if t[j] == "#":
+                        skipT += 1			# If not a valid character, keep times we must backspace.
+                        j = j - 1
+
+                    elif skipT > 0:
+                        skipT -= 1			# Backspace the number of times calculated in the previous step
+                        j = j - 1
+
+                    else:
+                        break
+
+            print("Comparing", s[i], t[j])		# Print out the characters for better understanding.
+
+            if i>= 0 and j >= 0 and s[i] != t[j]:    # Compare both valid characters. If not the same, return False.
+                return False
+
+            if (i>=0) != (j>=0):		# Also ensure that both the character indices are valid. If it is not valid,
+                return False			#  it means that we are comparing a "#" with a valid character.
+
+            i = i - 1
+            j = j - 1
+
+        return True					# This means both the strings are equivalent.
+
+            
+    def backspaceCompare_lazy_lst(self, s: str, t: str) -> bool:
+        def actual_string(s: str):
+            char_lst:List = []
+            for c in s:
+                if c != "#":
+                    char_lst.append(c)
+                else:
+                    char_lst.pop() if char_lst else ""
+            return "".join(char_lst)
+        return actual_string(s) == actual_string(t)
+
+# @lc code=end
+def main():
+    sol = Solution()
+    ans = sol.backspaceCompare(
+        "a#c", "b"
+    )
+    print(ans)
+
+
+if __name__ == "__main__":
+    main()
---
+++ need_review/15.3-sum.py
@@ -0,0 +1,88 @@
+#
+# @lc app=leetcode id=15 lang=python3
+#
+# [15] 3Sum
+#
+# https://leetcode.com/problems/3sum/description/
+#
+# algorithms
+# Medium (30.06%)
+# Likes:    14593
+# Dislikes: 1403
+# Total Accepted:    1.6M
+# Total Submissions: 5.4M
+# Testcase Example:  '[-1,0,1,2,-1,-4]'
+#
+# Given an integer array nums, return all the triplets [nums[i], nums[j],
+# nums[k]] such that i != j, i != k, and j != k, and nums[i] + nums[j] +
+# nums[k] == 0.
+#
+# Notice that the solution set must not contain duplicate triplets.
+#
+#
+# Example 1:
+# Input: nums = [-1,0,1,2,-1,-4]
+# Output: [[-1,-1,2],[-1,0,1]]
+# Example 2:
+# Input: nums = []
+# Output: []
+# Example 3:
+# Input: nums = [0]
+# Output: []
+#
+#
+# Constraints:
+#
+#
+# 0 <= nums.length <= 3000
+# -10^5 <= nums[i] <= 10^5
+#
+#
+#
+
+# @lc code=start
+from typing import List
+
+
+class Solution:
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+        """Hashmap 1 pass approach
+
+        There is also two pointer approach, using sorted list
+
+        """
+        hashmap = {}
+        found, dup_val = set(), set()
+
+        # O(n)
+        # for i, num in enumerate(nums):
+        #     hashmap[num] = i
+
+        # O(n^2)
+        for i, val_i in enumerate(nums):
+            # skip the iteration if the values already exist, (avoid a bunch of the same number)
+            if val_i not in dup_val:
+                dup_val.add(val_i)
+                for j, val_j in enumerate(nums[i + 1 :]):
+                    val_k = -(val_i + val_j)
+                    if val_k in hashmap and hashmap[val_k] == i:
+                            found.add(tuple(sorted([val_i, val_j, val_k])))
+                    hashmap[val_j] = i
+        return found
+
+
+# @lc code=end
+
+
+def main():
+    sol = Solution()
+    ans = sol.threeSum(
+        #
+        [-1, 0, 1, 2, -1, -4],
+        # [0]
+    )
+    print(ans)
+
+
+if __name__ == "__main__":
+    main()
