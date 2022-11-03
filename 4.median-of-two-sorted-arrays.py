7ae485f8dc7f8d7c9cd283fd1d21f44bc5de4570
Richard Huang <Xiaorui-Richard.Huang@ibm.com>
2022-11-02 21:31:27 -0400

    fix(4): fix edge cases
    while loop exiting immediately when l1 is length 1 and cover partition indexing
    for empty lists

--- 4.median-of-two-sorted-arrays.py
+++ 4.median-of-two-sorted-arrays.py
@@ -64,8 +64,8 @@ class Solution:
         is_odd = bool(total % 2)
 
         low, high = 0, len(l1) - 1
-        # (could also use while True, since it guarantees a medium)
-        while low <= high:
+        # since it guarantees a medium, we use while true
+        while True:
             partition_index = (low + high) // 2
             # from number of elements to index we subtract 1
             bound_index = target_half_size - (partition_index + 1) - 1
@@ -75,7 +75,7 @@ class Solution:
             # l2 : [...c | d ...]
             # b >= c and d >= a -> min(b, d) is the medium (for the odd case)
 
-            l1_left = l1[partition_index]
+            l1_left = l1[partition_index] if partition_index >= 0 else float("-inf")
             l1_right = l1[partition_index + 1] if (partition_index + 1) < len(l1) else float("inf")
             l2_left = l2[bound_index] if bound_index >= 0 else float("-inf")
             l2_right = l2[bound_index + 1] if (bound_index + 1) < len(l2) else float("inf")
@@ -91,7 +91,6 @@ class Solution:
             # last value of l2 is too big, need to increase the size of l1 partition
             else:  # (l2_left > l1_right)
                 low = partition_index + 1
-        print("an error occurred")
 
 
 # @lc code=end
@@ -105,8 +104,11 @@ def main():
 
     # 1,1,2,2,3,3,4, (5), 6,6,7,8,10,11,12
 
-    arr1 = [1, 2, 3, 5]
-    arr2 = [1, 2, 3, 4, 6, 6, 7, 8, 10, 11, 12]
+    # arr1 = [1, 2, 3, 5]
+    # arr2 = [1, 2, 3, 4, 6, 6, 7, 8, 10, 11, 12]
+
+    arr1 = []
+    arr2 = [1]
     ans = sol.findMedianSortedArrays(arr1, arr2)
     print(ans)
 
