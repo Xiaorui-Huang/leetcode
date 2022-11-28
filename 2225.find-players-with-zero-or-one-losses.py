0b018300a1c4cb44c8f603e601c90d62b50c9c78
Richard Huang <Xiaorui-Richard.Huang@ibm.com>
2022-11-28 00:44:52 -0500

    solve(2225): solve easy hash map problem
    Tags: Hash-Table

---
+++ 2225.find-players-with-zero-or-one-losses.py
@@ -0,0 +1,110 @@
+#
+# @lc app=leetcode id=2225 lang=python3
+#
+# [2225] Find Players With Zero or One Losses
+#
+# https://leetcode.com/problems/find-players-with-zero-or-one-losses/description/
+#
+# algorithms
+# Medium (69.29%)
+# Likes:    569
+# Dislikes: 49
+# Total Accepted:    40.2K
+# Total Submissions: 56.4K
+# Testcase Example:  '[[1,3],[2,3],[3,6],[5,6],[5,7],[4,5],[4,8],[4,9],[10,4],[10,9]]'
+#
+# You are given an integer array matches where matches[i] = [winneri, loseri]
+# indicates that the player winneri defeated player loseri in a match.
+#
+# Return a list answer of size 2 where:
+#
+#
+# answer[0] is a list of all players that have not lost any matches.
+# answer[1] is a list of all players that have lost exactly one match.
+#
+#
+# The values in the two lists should be returned in increasing order.
+#
+# Note:
+#
+#
+# You should only consider the players that have played at least one match.
+# The testcases will be generated such that no two matches will have the same
+# outcome.
+#
+#
+#
+# Example 1:
+#
+#
+# Input: matches =
+# [[1,3],[2,3],[3,6],[5,6],[5,7],[4,5],[4,8],[4,9],[10,4],[10,9]]
+# Output: [[1,2,10],[4,5,7,8]]
+# Explanation:
+# Players 1, 2, and 10 have not lost any matches.
+# Players 4, 5, 7, and 8 each have lost one match.
+# Players 3, 6, and 9 each have lost two matches.
+# Thus, answer[0] = [1,2,10] and answer[1] = [4,5,7,8].
+#
+#
+# Example 2:
+#
+#
+# Input: matches = [[2,3],[1,3],[5,4],[6,4]]
+# Output: [[1,2,5,6],[]]
+# Explanation:
+# Players 1, 2, 5, and 6 have not lost any matches.
+# Players 3 and 4 each have lost two matches.
+# Thus, answer[0] = [1,2,5,6] and answer[1] = [].
+#
+#
+#
+# Constraints:
+#
+#
+# 1 <= matches.length <= 10^5
+# matches[i].length == 2
+# 1 <= winneri, loseri <= 10^5
+# winneri != loseri
+# All matches[i] are unique.
+#
+#
+#
+
+# @lc code=start
+from bisect import insort
+
+
+class Solution:
+    def findWinners(self, matches: list[list[int]]) -> list[list[int]]:
+        records = {}
+        undefeated = []
+        lost_once = []
+        for match in matches:
+            winner, loser = match
+            if winner not in records:
+                records[winner] = 0
+            records[loser] = records.get(loser, 0) + 1
+
+        for team, losses in records.items():
+            if losses == 0:
+                insort(undefeated, team)
+            if losses == 1:
+                insort(lost_once, team)
+
+        return [undefeated, lost_once]
+
+
+# @lc code=end
+
+
+def main():
+    sol = Solution()
+    # matches = [[1, 3], [2, 3], [3, 6], [5, 6], [5, 7], [4, 5], [4, 8], [4, 9], [10, 4], [10, 9]]
+    matches = [[2, 3], [1, 3], [5, 4], [6, 4]]
+    ans = sol.findWinners(matches)
+    print(ans)
+
+
+if __name__ == "__main__":
+    main()
