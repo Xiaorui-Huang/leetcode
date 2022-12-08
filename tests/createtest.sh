2320268039beb4240d79e2d05f7b565420978e82
Richard Huang <Xiaorui-Richard.Huang@ibm.com>
2022-12-08 18:00:47 -0500

    chores(test): stylistic change

--- tests/createtest.sh
+++ tests/createtest.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+# enables create pytest tasks in vscode in combination with .vscode/tasks.json
 
 if [ $# != 1 ]
 then
@@ -7,17 +8,22 @@ then
 fi
 
 number=$1
-regex="\.\.\/python3\/lc_(.+)\.py"
+regex="\.\.\/python3\/lc_(.+)\.py" # regex to match prolem file name e.g. ../python3/lc_34_easy_stupid_question.py
 
+# check if original question exists first
 if name=$(ls ../python3/lc_${number}_* 2> /dev/null)
 then
     if [[ $name =~ $regex ]]
     then
+        # match and get file name
         name="${BASH_REMATCH[1]}"
         touch test_${name}.py
         echo "created test_${name}.py"
     else
-        echo "didn't match"
+        echo "regex didn't match original question file"
+        exit 1
     fi
+else
+    echo "leetcode question not in local repository"
+    exit 1
 fi
-
