621930f28ad3233a40b5b8fee0d5a976f03f3d20
Richard Huang <Xiaorui-Richard.Huang@ibm.com>
2022-12-19 15:22:14 -0500

    chore(test): to be squashed

--- cpp_tests/test_486_medium_predict_the_winner.cpp
+++ cpp_tests/test_486_medium_predict_the_winner.cpp
@@ -1,7 +1,7 @@
 #include "lc_486_medium_predict_the_winner.cpp"
 #include <gtest/gtest.h>
 
-TEST(LeetCode, PredictTheWinner) {
+TEST(LeetCode, lc_486_medium_predict_the_winner) {
     auto sol = Solution();
     std::vector<int> arr{1, 5, 233, 7};
     EXPECT_TRUE(sol.PredictTheWinner(arr));
