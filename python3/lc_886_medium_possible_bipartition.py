648e10c1f7848575cc56de420cd6368a8bf8c3c2
Richard Huang <Xiaorui-Richard.Huang@ibm.com>
2022-12-21 11:49:59 -0500

    approach(886): direct access approach instead of hash sets
    Tags: Direct-Address-Table

--- python3/lc_886_medium_possible_bipartition.py
+++ python3/lc_886_medium_possible_bipartition.py
@@ -60,8 +60,58 @@
 #
 
 # @lc code=start
+from enum import Enum
+from typing import Callable
+
+appr = Enum("approaches", "set direct")
+APPR = appr.direct
+BLACK = 0
+WHITE = 1
+GREY = -1
+
+
 class Solution:
     def possibleBipartition(self, n: int, dislikes: list[list[int]]) -> bool:
+        if APPR == appr.set:
+            return self.possibleBipartition_set(n, dislikes)
+        if APPR == appr.direct:
+            return self.possibleBipartition_direct(n, dislikes)
+        return False  # Never Reached
+
+    def possibleBipartition_direct(self, n: int, dislikes: list[list[int]]) -> bool:
+        graph: list[list[int]] = [[] for _ in range(n + 1)]
+        for a, b in dislikes:
+            graph[a].append(b)
+            graph[b].append(a)
+
+        other: Callable[[int], int] = lambda color: color ^ 1  # XOR flips color
+        color = [GREY] * (n + 1)
+
+        def dfs(node: int) -> bool:
+            stack = [node]
+            while stack:
+                cur = stack.pop()
+                for neighbor in graph[cur]:
+                    # the color is the same -> cannot be 2 colorable
+                    if color[neighbor] == color[cur]:
+                        return False
+                    # not colored -> so we color the neighbor
+                    if color[neighbor] == GREY:
+                        color[neighbor] = other(color[cur])
+                        stack.append(neighbor)
+            return True
+
+        for node in range(1, n + 1):
+            if color[node] == GREY:
+                # assign red as default
+                color[node] = BLACK
+                # see if 2 coloring this results in coloring conflict
+                if not dfs(node):
+                    return False
+
+        return True
+
+    def possibleBipartition_set(self, n: int, dislikes: list[list[int]]) -> bool:
         graph: dict[int, list[int]] = {}
         for a, b in dislikes:
             graph.setdefault(a, []).append(b)
