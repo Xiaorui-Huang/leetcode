b2a065f6ab24e36296e7154f7a89d5026372d824
Richard Huang <Xiaorui-Richard.Huang@ibm.com>
2023-01-06 17:14:14 -0500

    test(rust): rust test setup

--- src/bin/lc_1_easy_two_sum.rs
+++ src/bin/lc_1_easy_two_sum.rs
@@ -15,46 +15,46 @@
  *
  * Given an array of integers nums and an integer target, return indices of the
  * two numbers such that they add up to target.
- * 
+ *
  * You may assume that each input would have exactly one solution, and you may
  * not use the same element twice.
- * 
+ *
  * You can return the answer in any order.
- * 
- * 
+ *
+ *
  * Example 1:
- * 
- * 
+ *
+ *
  * Input: nums = [2,7,11,15], target = 9
  * Output: [0,1]
  * Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
- * 
- * 
+ *
+ *
  * Example 2:
- * 
- * 
+ *
+ *
  * Input: nums = [3,2,4], target = 6
  * Output: [1,2]
- * 
- * 
+ *
+ *
  * Example 3:
- * 
- * 
+ *
+ *
  * Input: nums = [3,3], target = 6
  * Output: [0,1]
- * 
- * 
- * 
+ *
+ *
+ *
  * Constraints:
- * 
- * 
+ *
+ *
  * 2 <= nums.length <= 10^4
  * -10^9 <= nums[i] <= 10^9
  * -10^9 <= target <= 10^9
  * Only one valid answer exists.
- * 
- * 
- * 
+ *
+ *
+ *
  * Follow-up: Can you come up with an algorithm that is less than O(n^2) time
  * complexity?
  */
@@ -66,9 +66,11 @@ impl Solution {
         use std::collections::HashMap;
         let mut mem = HashMap::new();
 
-        for (i, num) in nums.iter().enumerate(){
+        for (i, num) in nums.iter().enumerate() {
             let complement = target - num;
-            if mem.contains_key(&complement) {return vec![i as i32, *mem.get(&complement).unwrap() as i32]}
+            if mem.contains_key(&complement) {
+                return vec![i as i32, *mem.get(&complement).unwrap() as i32];
+            }
             mem.insert(num, i);
         }
 
@@ -77,10 +79,21 @@ impl Solution {
 }
 // @lc code=end
 
-fn main(){
-    let ans = Solution::two_sum(vec![2,7,11,15], 9);
+use rstest::rstest;
+
+#[rstest]
+#[case(vec![2,7,11,15,9], 9, vec![1,0])]
+#[case(vec![3,2,4], 6, vec![2,1])]
+#[case(vec![3,3], 6, vec![0,1])]
+fn test_two_sum(#[case] nums: Vec<i32>, #[case] target: i32, #[case] expected: Vec<i32>) {
+    let ans = Solution::two_sum(nums, target);
+    assert!(ans == expected || vec![ans[1], ans[0]] == expected);
+}
+
+fn main() {
+    let ans = Solution::two_sum(vec![2, 7, 11, 15], 9);
     println!("{:?}", ans);
-    
-    let ans = Solution::two_sum(vec![3,2,4], 6);
+
+    let ans = Solution::two_sum(vec![3, 2, 4], 6);
     println!("{:?}", ans);
 }
