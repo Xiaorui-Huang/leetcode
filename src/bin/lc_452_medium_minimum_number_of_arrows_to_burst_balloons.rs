e24b08f965ae9dbbb3bfbf9b5f3b870676fe66e3
Richard Huang <Xiaorui-Richard.Huang@ibm.com>
2023-01-07 12:00:20 -0500

    solve(452): min arrow shoot balloons - medium
    rephrasing of largest non-overlapping algorithm
    
    Tags: Greedy

---
+++ src/bin/lc_452_medium_minimum_number_of_arrows_to_burst_balloons.rs
@@ -0,0 +1,105 @@
+/*
+ * @lc app=leetcode id=452 lang=rust
+ *
+ * [452] Minimum Number of Arrows to Burst Balloons
+ *
+ * https://leetcode.com/problems/minimum-number-of-arrows-to-burst-balloons/description/
+ *
+ * algorithms
+ * Medium (53.19%)
+ * Likes:    5710
+ * Dislikes: 157
+ * Total Accepted:    289.2K
+ * Total Submissions: 522.8K
+ * Testcase Example:  '[[10,16],[2,8],[1,6],[7,12]]'
+ *
+ * There are some spherical balloons taped onto a flat wall that represents the
+ * XY-plane. The balloons are represented as a 2D integer array points where
+ * points[i] = [xstart, xend] denotes a balloon whose horizontal diameter
+ * stretches between xstart and xend. You do not know the exact y-coordinates
+ * of the balloons.
+ *
+ * Arrows can be shot up directly vertically (in the positive y-direction) from
+ * different points along the x-axis. A balloon with xstart and xend is burst
+ * by an arrow shot at x if xstart <= x <= xend. There is no limit to the
+ * number of arrows that can be shot. A shot arrow keeps traveling up
+ * infinitely, bursting any balloons in its path.
+ *
+ * Given the array points, return the minimum number of arrows that must be
+ * shot to burst all balloons.
+ *
+ *
+ * Example 1:
+ *
+ *
+ * Input: points = [[10,16],[2,8],[1,6],[7,12]]
+ * Output: 2
+ * Explanation: The balloons can be burst by 2 arrows:
+ * - Shoot an arrow at x = 6, bursting the balloons [2,8] and [1,6].
+ * - Shoot an arrow at x = 11, bursting the balloons [10,16] and [7,12].
+ *
+ *
+ * Example 2:
+ *
+ *
+ * Input: points = [[1,2],[3,4],[5,6],[7,8]]
+ * Output: 4
+ * Explanation: One arrow needs to be shot for each balloon for a total of 4
+ * arrows.
+ *
+ *
+ * Example 3:
+ *
+ *
+ * Input: points = [[1,2],[2,3],[3,4],[4,5]]
+ * Output: 2
+ * Explanation: The balloons can be burst by 2 arrows:
+ * - Shoot an arrow at x = 2, bursting the balloons [1,2] and [2,3].
+ * - Shoot an arrow at x = 4, bursting the balloons [3,4] and [4,5].
+ *
+ *
+ *
+ * Constraints:
+ *
+ *
+ * 1 <= points.length <= 10^5
+ * points[i].length == 2
+ * -2^31 <= xstart < xend <= 2^31 - 1
+ *
+ *
+ */
+
+struct Solution;
+// @lc code=start
+impl Solution {
+    pub fn find_min_arrow_shots(points: Vec<Vec<i32>>) -> i32 {
+        let mut points = points;
+        points.sort_by(|a, b| a[1].cmp(&b[1]));
+
+        let mut count = 1;
+        let mut arrow_pos = points[0][1];
+
+        for point in points {
+            let (start, end) = (point[0], point[1]);
+            if arrow_pos >= start {
+                continue;
+            }
+            count += 1;
+            arrow_pos = end;
+        }
+        count
+    }
+}
+// @lc code=end
+
+use rstest::rstest;
+
+#[rstest]
+#[case([[10,16],[2,8],[1,6],[7,12]].iter().map(|x|x.to_vec()).collect(), 2)]
+#[case([[1,2],[3,4],[5,6],[7,8]].iter().map(|x|x.to_vec()).collect(), 4)]
+#[case([[1,2],[2,3],[3,4],[4,5]].iter().map(|x|x.to_vec()).collect(), 2)]
+fn test_find_min_arrow_shots(#[case] points: Vec<Vec<i32>>, #[case] expected: i32) {
+    assert_eq!(Solution::find_min_arrow_shots(points), expected);
+}
+
+fn main() {}
