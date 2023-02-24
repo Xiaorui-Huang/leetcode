cc16d14f3dbf8da98be7216d8fe499d5c03ef6fe
Richard Huang <hxr.richard@gmail.com>
2023-02-24 11:09:12 -0500

    rust(226): invert binary tree - easy
    learning how to do tree problem in rust
    
    Tags: Binary-Tree

---
+++ src/bin/lc_226_easy_invert_binary_tree.rs
@@ -0,0 +1,87 @@
+/*
+ * @lc app=leetcode id=226 lang=rust
+ *
+ * [226] Invert Binary Tree
+ *
+ * https://leetcode.com/problems/invert-binary-tree/description/
+ *
+ * algorithms
+ * Easy (73.67%)
+ * Likes:    11733
+ * Dislikes: 166
+ * Total Accepted:    1.5M
+ * Total Submissions: 2M
+ * Testcase Example:  '[4,2,7,1,3,6,9]'
+ *
+ * Given the root of a binary tree, invert the tree, and return its root.
+ *
+ *
+ * Example 1:
+ *
+ *
+ * Input: root = [4,2,7,1,3,6,9]
+ * Output: [4,7,2,9,6,3,1]
+ *
+ *
+ * Example 2:
+ *
+ *
+ * Input: root = [2,1,3]
+ * Output: [2,3,1]
+ *
+ *
+ * Example 3:
+ *
+ *
+ * Input: root = []
+ * Output: []
+ *
+ *
+ *
+ * Constraints:
+ *
+ *
+ * The number of nodes in the tree is in the range [0, 100].
+ * -100 <= Node.val <= 100
+ *
+ *
+ */
+
+// Definition for a binary tree node.
+#[derive(Debug, PartialEq, Eq)]
+pub struct TreeNode {
+    pub val: i32,
+    pub left: Option<Rc<RefCell<TreeNode>>>,
+    pub right: Option<Rc<RefCell<TreeNode>>>,
+}
+
+impl TreeNode {
+    #[inline]
+    pub fn new(val: i32) -> Self {
+        TreeNode {
+            val,
+            left: None,
+            right: None,
+        }
+    }
+}
+
+struct Solution;
+// @lc code=start
+use std::cell::RefCell;
+use std::rc::Rc;
+impl Solution {
+    pub fn invert_tree(root: Option<Rc<RefCell<TreeNode>>>) -> Option<Rc<RefCell<TreeNode>>> {
+        if let Some(ref node) = root {
+            let mut node = node.borrow_mut();
+            let left = node.left.take();
+            let right = node.right.take();
+            node.left = Self::invert_tree(right);
+            node.right = Self::invert_tree(left);
+        }
+        return root;
+    }
+}
+// @lc code=end
+
+fn main() {}
