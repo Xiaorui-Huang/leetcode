a19ca34a23372ebc13cb7185b99594765dfd5a31
Richard-Xiaorui Huang <hxr.richard@gmail.com>
2024-03-20 17:12:53 -0400

    rename(57)

--- cpp/lc_57_medium_insert_interval.cpp
+++ cpp/lc_57_medium_insert_interval.cpp
@@ -1,12 +1,20 @@
-/**
+/*
  * @lc app=leetcode id=57 lang=cpp
- * URL: https://leetcode.com/problems/insert-interval/description/
  *
- * 57. Insert Interval
+ * [57] Insert Interval
  *
+ * https://leetcode.com/problems/insert-interval/description/
+ *
+ * algorithms
+ * Medium (41.33%)
+ * Likes:    10106
+ * Dislikes: 776
+ * Total Accepted:    1.1M
+ * Total Submissions: 2.6M
+ * Testcase Example:  '[[1,3],[6,9]]\n[2,5]'
  *
  * You are given an array of non-overlapping intervals intervals where
- * intervals[i] = [starti, endi] represent the start and the end of the ith
+ * intervals[i] = [starti, endi] represent the start and the end of the i^th
  * interval and intervals is sorted in ascending order by starti. You are also
  * given an interval newInterval = [start, end] that represents the start and
  * end of another interval.
@@ -15,32 +23,40 @@
  * ascending order by starti and intervals still does not have any overlapping
  * intervals (merge overlapping intervals if necessary).
  *
- *
  * Return intervals after the insertion.
  *
  * Note that you don't need to modify intervals in-place. You can make a new
  * array and return it.
  *
- *   Example 1:
+ *
+ * Example 1:
+ *
  *
  * Input: intervals = [[1,3],[6,9]], newInterval = [2,5]
  * Output: [[1,5],[6,9]]
  *
+ *
  * Example 2:
+ *
  *
  * Input: intervals = [[1,2],[3,5],[6,7],[8,10],[12,16]], newInterval = [4,8]
  * Output: [[1,2],[3,10],[12,16]]
- * Explanation: Because the new interval [4,8] overlaps with [3,5],[6,7],[8,10].
+ * Explanation: Because the new interval [4,8] overlaps with
+ * [3,5],[6,7],[8,10].
+ *
+ *
  *
  * Constraints:
  *
  *
- *  0 <= intervals.length <= 104
- *  intervals[i].length == 2
- *  0 <= starti <= endi <= 105
- *  intervals is sorted by starti in ascending order.
- *  newInterval.length == 2
- *	0 <= start <= end <= 105
+ * 0 <= intervals.length <= 10^4
+ * intervals[i].length == 2
+ * 0 <= starti <= endi <= 10^5
+ * intervals is sorted by starti in ascending order.
+ * newInterval.length == 2
+ * 0 <= start <= end <= 10^5
+ *
+ *
  */
 #include <algorithm>
 #include <cassert>
