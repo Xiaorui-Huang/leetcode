61da2ca60dcaa682616a79fef8f7aaad23465036
Richard-Xiaorui Huang <hxr.richard@gmail.com>
2024-03-21 00:31:43 -0400

    solve(206): easy reverse linked tree

---
+++ cpp/lc_206_easy_reverse_linked_list.cpp
@@ -0,0 +1,94 @@
+/*
+ * @lc app=leetcode id=206 lang=cpp
+ *
+ * [206] Reverse Linked List
+ *
+ * https://leetcode.com/problems/reverse-linked-list/description/
+ *
+ * algorithms
+ * Easy (75.86%)
+ * Likes:    20846
+ * Dislikes: 410
+ * Total Accepted:    3.9M
+ * Total Submissions: 5.1M
+ * Testcase Example:  '[1,2,3,4,5]'
+ *
+ * Given the head of a singly linked list, reverse the list, and return the
+ * reversed list.
+ *
+ *
+ * Example 1:
+ *
+ *
+ * Input: head = [1,2,3,4,5]
+ * Output: [5,4,3,2,1]
+ *
+ *
+ * Example 2:
+ *
+ *
+ * Input: head = [1,2]
+ * Output: [2,1]
+ *
+ *
+ * Example 3:
+ *
+ *
+ * Input: head = []
+ * Output: []
+ *
+ *
+ *
+ * Constraints:
+ *
+ *
+ * The number of nodes in the list is the range [0, 5000].
+ * -5000 <= Node.val <= 5000
+ *
+ *
+ *
+ * Follow up: A linked list can be reversed either iteratively or recursively.
+ * Could you implement both?
+ *
+ */
+
+#include <algorithm>
+#include <cmath>
+#include <iostream>
+#include <limits>
+#include <unordered_map>
+#include <unordered_set>
+#include <vector>
+using namespace std;
+
+#include "linked_list.h"
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+  public:
+    ListNode *reverseList(ListNode *head) {
+        ListNode *prev = NULL, *next = NULL;
+        while (head) {
+            // save the next node
+            next = head->next;
+            // connect backwards
+            head->next = prev;
+            // save prev
+            prev = head;
+            // pop the next node
+            head = next;
+        }
+        return prev;
+    }
+};
+// @lc code=end
