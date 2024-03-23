de463195f2cf2a5b356b763141641c20af792e95
Richard-Xiaorui Huang <hxr.richard@gmail.com>
2024-03-23 02:46:15 -0400

    solve(133): clone graph c++

---
+++ cpp/lc_133_medium_clone_graph.cpp
@@ -0,0 +1,156 @@
+/*
+ * @lc app=leetcode id=133 lang=cpp
+ *
+ * [133] Clone Graph
+ *
+ * https://leetcode.com/problems/clone-graph/description/
+ *
+ * algorithms
+ * Medium (56.82%)
+ * Likes:    9261
+ * Dislikes: 3721
+ * Total Accepted:    1.2M
+ * Total Submissions: 2.1M
+ * Testcase Example:  '[[2,4],[1,3],[2,4],[1,3]]'
+ *
+ * Given a reference of a node in a connected undirected graph.
+ *
+ * Return a deep copy (clone) of the graph.
+ *
+ * Each node in the graph contains a value (int) and a list (List[Node]) of its
+ * neighbors.
+ *
+ *
+ * class Node {
+ * ⁠   public int val;
+ * ⁠   public List<Node> neighbors;
+ * }
+ *
+ *
+ *
+ *
+ * Test case format:
+ *
+ * For simplicity, each node's value is the same as the node's index
+ * (1-indexed). For example, the first node with val == 1, the second node with
+ * val == 2, and so on. The graph is represented in the test case using an
+ * adjacency list.
+ *
+ * An adjacency list is a collection of unordered lists used to represent a
+ * finite graph. Each list describes the set of neighbors of a node in the
+ * graph.
+ *
+ * The given node will always be the first node with val = 1. You must return
+ * the copy of the given node as a reference to the cloned graph.
+ *
+ *
+ * Example 1:
+ *
+ *
+ * Input: adjList = [[2,4],[1,3],[2,4],[1,3]]
+ * Output: [[2,4],[1,3],[2,4],[1,3]]
+ * Explanation: There are 4 nodes in the graph.
+ * 1st node (val = 1)'s neighbors are 2nd node (val = 2) and 4th node (val =
+ * 4).
+ * 2nd node (val = 2)'s neighbors are 1st node (val = 1) and 3rd node (val =
+ * 3).
+ * 3rd node (val = 3)'s neighbors are 2nd node (val = 2) and 4th node (val =
+ * 4).
+ * 4th node (val = 4)'s neighbors are 1st node (val = 1) and 3rd node (val =
+ * 3).
+ *
+ *
+ * Example 2:
+ *
+ *
+ * Input: adjList = [[]]
+ * Output: [[]]
+ * Explanation: Note that the input contains one empty list. The graph consists
+ * of only one node with val = 1 and it does not have any neighbors.
+ *
+ *
+ * Example 3:
+ *
+ *
+ * Input: adjList = []
+ * Output: []
+ * Explanation: This an empty graph, it does not have any nodes.
+ *
+ *
+ *
+ * Constraints:
+ *
+ *
+ * The number of nodes in the graph is in the range [0, 100].
+ * 1 <= Node.val <= 100
+ * Node.val is unique for each node.
+ * There are no repeated edges and no self-loops in the graph.
+ * The Graph is connected and all nodes can be visited starting from the given
+ * node.
+ *
+ *
+ */
+#include <algorithm>
+#include <cmath>
+#include <iostream>
+#include <limits>
+#include <queue>
+#include <unordered_map>
+#include <unordered_set>
+#include <vector>
+using namespace std;
+
+// Definition for a Node.
+class Node {
+  public:
+    int val;
+    vector<Node *> neighbors;
+    Node() {
+        val = 0;
+        neighbors = vector<Node *>();
+    }
+    Node(int _val) {
+        val = _val;
+        neighbors = vector<Node *>();
+    }
+    Node(int _val, vector<Node *> _neighbors) {
+        val = _val;
+        neighbors = _neighbors;
+    }
+};
+
+// @lc code=start
+class Solution {
+  public:
+    unordered_map<int, Node *> nodes_map;
+
+    Node *get_node_clone(Node *node) {
+        auto node_it = nodes_map.find(node->val);
+        auto *node_clone = (node_it != nodes_map.end()) ? node_it->second
+                                                        : new Node(node->val);
+        return node_clone;
+    }
+    Node *cloneGraph(Node *node) {
+        if (!node)
+            return NULL;
+        queue<Node *> q;
+
+        q.push(node);
+        while (!q.empty()) {
+            auto node = q.front();
+            q.pop();
+            auto *node_clone = get_node_clone(node);
+
+            for (auto &neighbor : node->neighbors) {
+                if (!nodes_map.count(neighbor->val))
+                    q.push(neighbor);
+
+                auto neighbor_clone = get_node_clone(neighbor);
+                node_clone->neighbors.push_back(neighbor_clone);
+            }
+        }
+
+        return nodes_map.at(1);
+    }
+};
+// @lc code=end
