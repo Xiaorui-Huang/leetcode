185b202c61b71dfb32278b68b6c2f8a9515bf81c
Richard-Xiaorui Huang <hxr.richard@gmail.com>
2024-09-01 04:34:24 -0400

    chore: Add quick_sort.h to C settings.json

--- c/lc_2022_easy_convert_1_d_array_into_2_d_array.c
+++ c/lc_2022_easy_convert_1_d_array_into_2_d_array.c
@@ -68,8 +68,10 @@
  * 
  * 
  */
+#include <stdlib.h>
 
 // @lc code=start
+
 /**
  * Return an array of arrays of size *returnSize.
  * The sizes of the arrays are returned as *returnColumnSizes array.
